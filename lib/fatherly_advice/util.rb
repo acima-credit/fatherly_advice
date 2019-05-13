6eec820cafa45d926feeb0a38be7c1afe9700ba5
Adrian Esteban Madrid <aemadrid@gmail.com>
2019-05-13 17:51:15 -0600

    Adds default filters.

--- lib/fatherly_advice/util.rb
+++ lib/fatherly_advice/util.rb
@@ -28,6 +28,23 @@ module FatherlyAdvice
       def safe_arg_hash(hsh)
         ::ActionDispatch::Http::ParameterFilter.new(WebServer.parameter_filters).filter hsh
       end
+
+      def default_filters
+        %i[
+          account_name account_number acct_no address address_1 address_2 alt_phone api_access_token
+          authorization_token card_number cell_phone city corrected_employer_phone cvv date_of_birth_day
+          date_of_birth_month date_of_birth_year dba dl_number dob email employer_2_name employer_2_phone
+          employer_name employer_phone encrypted_password encrypted_password_iv expiry first_name home_phone
+          identification_number identity_card_number identity_number last_name legal_business_name
+          mailing_address_1 mailing_address_2 main_phone mobile_phone name new_account_number
+          new_routing_number password password_confirmation password_digest phone_number previous_address_1
+          previous_address_2 reference_1_name reference_1_phone reference_2_name reference_2_phone
+          reset_password_token routing_no routing_number secondary_phone_number secret ssn street_address
+          token user user_token zip
+        ]
+      end
+
+      module_function :default_filters
     end
 
     module SafeDependencies
